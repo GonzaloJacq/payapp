a04c9fac19e8fac855ac75d24e96a3bb99e5a90f
gjacques <gjacques@tessa.com.uy>
2025-08-05 17:39:59 -0300

    fix: center login y deniedAccesPage

--- src/features/components/deniedAccess.tsx
+++ src/features/components/deniedAccess.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import { Body } from "./body";
+import Footer from "../../footer";
 
 export default function DeniedAcces() {
   const navigate = useNavigate();
@@ -9,16 +10,19 @@ export default function DeniedAcces() {
   };
 
   return (
-    <Body>
-      <div className="min-h-screen flex flex-col p-4">
-        <h1 className="text-2xl text-red-600">Acceso denegado</h1>
-        <button
-          onClick={handleRedirect}
-          className="cursor-pointer hover:text-blue-500 transition"
-        >
-          Volver al Login
-        </button>
-      </div>
-    </Body>
+    <div className="min-h-screen flex flex-col">
+      <Body>
+        <div className="flex flex-1 flex-col items-center justify-center text-center p-4">
+          <h1 className="text-2xl text-red-600 mb-4">Acceso denegado</h1>
+          <button
+            onClick={handleRedirect}
+            className="cursor-pointer text-blue-600 hover:underline transition"
+          >
+            Volver al Login
+          </button>
+        </div>
+      </Body>
+      <Footer />
+    </div>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import { useAuthStore } from "../features/store/user.store";
 import { useShallow } from "zustand/shallow";
+import Footer from "../footer";
 type FormInputs = {
   user: string;
   password: string;
@@ -28,67 +29,75 @@ export default function Login() {
   };
 
   return (
-    <Body>
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-center items-center">
-          <img src={moneyLogo} className="h-32 w-32 " alt="React logo" />
+    <div className="min-h-screen flex flex-col">
+      <Body>
+        <div className="flex-1 flex items-center justify-center p-4">
+          <div className="flex flex-col gap-4 max-w-md w-full">
+            <div className="flex justify-center items-center">
+              <img src={moneyLogo} className="h-32 w-32 " alt="React logo" />
+            </div>
+            <h1 className="text-5xl">My Payapp</h1>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="gap-4 flex flex-col"
+            >
+              <div>
+                <input
+                  className="w-full p-2 border rounded"
+                  step={"0.01"}
+                  min="0"
+                  type="text"
+                  id="user"
+                  {...register("user", {
+                    required: "Ingrese el nombre de usuario",
+                    min: {
+                      value: 3,
+                      message: "Debe contener al menos 3 caracteres",
+                    },
+                  })}
+                />
+                {errors.user && (
+                  <p className="text-red-600 mt-1">{errors.user.message}</p>
+                )}
+              </div>
+              <div>
+                <input
+                  className="w-full p-2 border rounded"
+                  step={"0.01"}
+                  min="0"
+                  type="password"
+                  id="password"
+                  {...register("password", {
+                    required: "Ingrese el nombre la contraseña",
+                    min: {
+                      value: 3,
+                      message: "Debe contener al menos 3 caracteres",
+                    },
+                  })}
+                />
+              </div>
+              {errors.password && (
+                <p className="text-red-600 mt-1">{errors.password.message}</p>
+              )}
+              <div className="justify-between flex">
+                <button
+                  type="button"
+                  className="hover:text-blue-500 cursor-pointer transition"
+                >
+                  registrarse
+                </button>
+                <button
+                  type="submit"
+                  className="bg-green-600 w-16 text-white py-2 rounded hover:bg-green-700 transition cursor-pointer"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <h1 className="text-5xl">My Payapp</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="gap-4 flex flex-col">
-          <div>
-            <input
-              className="w-full p-2 border rounded"
-              step={"0.01"}
-              min="0"
-              type="text"
-              id="user"
-              {...register("user", {
-                required: "Ingrese el nombre de usuario",
-                min: {
-                  value: 3,
-                  message: "Debe contener al menos 3 caracteres",
-                },
-              })}
-            />
-            {errors.user && (
-              <p className="text-red-600 mt-1">{errors.user.message}</p>
-            )}
-          </div>
-          <div>
-            <input
-              className="w-full p-2 border rounded"
-              step={"0.01"}
-              min="0"
-              type="password"
-              id="password"
-              {...register("password", {
-                required: "Ingrese el nombre la contraseña",
-                min: {
-                  value: 3,
-                  message: "Debe contener al menos 3 caracteres",
-                },
-              })}
-            />
-          </div>
-          {errors.password && (
-            <p className="text-red-600 mt-1">{errors.password.message}</p>
-          )}
-          <div className="justify-between flex">
-            <button
-              type="button"
-              className="hover:text-blue-500 cursor-pointer transition"
-            >
-              registrarse
-            </button>
-            <button
-              type="submit"
-              className="bg-green-600 w-16 text-white py-2 rounded hover:bg-green-700 transition cursor-pointer"
-            >
-              Login
-            </button>
-          </div>
-        </form>
-      </div>
-    </Body>
+      </Body>
+      <Footer />
+    </div>
   );
 }
